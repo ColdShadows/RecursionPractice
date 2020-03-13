f18e6237bfe72825876858080a69e4385dd13206
Travis Kean <traviskean@nexsystech.com>
2020-03-13 15:35:06 -0400

    Created Solution for Recursion Practice. Added Extension for int to get the power of the integer.
    Created test project, with associated tests for GetPowerOf method
    
    Added .gitignore file

---
+++ RecursionPractice/RecursionPractice/IntExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace RecursionPractice
+{
+    public static class IntExtensions
+    {
+        public static int GetPowerOf(this int number, int power)
+        {
+            //Exit Condition
+            if(power == 0)
+            {
+                return 1;
+            }
+            //Exit Condition
+            if (power == 1)
+            {
+                return number;
+            }
+
+            return number * GetPowerOf(number, power - 1);
+        }
+        
+    }
+}
---
+++ RecursionPractice/RecursionPracticeTests/IntExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace RecursionPractice
+{
+    public class IntExtensionsTests
+    {
+        [Theory]
+        [InlineData(2, 2, 4)]
+        [InlineData(4, 0, 1)]
+        [InlineData(1, 8, 1)]
+        [InlineData(4, 1, 4)]
+        [InlineData(2, 5, 32)]
+        public void GetPowerOf_GivenNumberAndPower_ReturnMatchesExpectedValue(int number, int power, int expected)
+        {
+            var result = number.GetPowerOf(power);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
